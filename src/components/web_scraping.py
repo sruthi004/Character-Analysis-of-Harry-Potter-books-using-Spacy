8267c1b4ad6707e8efde26a5939fc0f8d2330837
SS <sruthirajpandian@gmail.com>
2023-09-23 20:01:31 -0400

    'files'

--- src/components/web_scraping.py
+++ src/components/web_scraping.py
@@ -6,5 +6,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 # Creating web driver object
 driver = webdriver.Chrome()
 page_url = "https://shadowhunters.fandom.com/wiki/Chain_of_Gold"
-
 driver.get(page_url)
+
+# Finding specific elements from the page (If we have cookie)
+# from selenium.webdriver.common.by import By
+# driver.find_element()
+
+# 
