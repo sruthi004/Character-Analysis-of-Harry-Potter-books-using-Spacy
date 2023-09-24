8f7772e79a906ebf7c21fffa26c4e909a48c2235
SS <sruthirajpandian@gmail.com>
2023-09-23 21:10:44 -0400

    'web_scraping'

--- src/components/web_scraping.py
+++ src/components/web_scraping.py
@@ -5,11 +5,17 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 # Creating web driver object
 driver = webdriver.Chrome()
-page_url = "https://shadowhunters.fandom.com/wiki/Chain_of_Gold"
+page_url = "https://harrypotter.fandom.com/wiki/Category:Character_indexes"
 driver.get(page_url)
 
 # Finding specific elements from the page (If we have cookie)
 # from selenium.webdriver.common.by import By
 # driver.find_element()
 
-# 
+# Extracting data from web pages
+# Books
+from selenium.webdriver.common.by import By
+books = driver.find_elements(By.CLASS_NAME, 'category-page__member-link') 
+#print(books[6].text) # Books got extracted
+#print(books[0].get_attribute('href')) # Links for books
+driver.get(books[0].get_attribute('href')) # Access the link
