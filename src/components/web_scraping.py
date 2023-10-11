45148bbf86b2693d42028f78dc04f4f25735c4ed
SS <sruthirajpandian@gmail.com>
2023-10-10 20:34:10 -0400

    'files'

--- src/components/web_scraping.py
+++ src/components/web_scraping.py
@@ -19,14 +19,25 @@ books = driver.find_elements(By.CLASS_NAME, 'category-page__member-link')
 #print(books[6].text) # Books got extracted
 #print(books[0].get_attribute('href')) # Links for books
 time.sleep(3)
-driver.get(books[0].get_attribute('href')) # Access the link
-characters = driver.find_elements(By.CLASS_NAME, 'article-table') 
+#driver.get(books[0].get_attribute('href')) # Access the link
+#characters = driver.find_elements(By.CLASS_NAME, 'article-table') 
 #print(characters[1].text)
 
 # Creating a dictionary of books and URLs
 books_url_dict = []
 for category in books:
-    book_url = driver.get(category.get_attribute('href'))
+    book_url = category.get_attribute('href')
     book_name = category.text
     books_url_dict.append({'book_name':book_name, 'url':book_url})
 print(books_url_dict)
+
+# Extract characters
+char_list = []
+for book in books_url_dict:
+    # go to book page
+    driver.get(book['url'])
+    
+    char_elems = driver.find_elements(By.CLASS_NAME, 'article-table')
+    for elem in char_elems:
+        char_list.append({'book':book['book_name'],'character':elem.text})
+char_list
